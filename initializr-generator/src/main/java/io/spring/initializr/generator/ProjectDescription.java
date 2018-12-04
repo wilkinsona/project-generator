19655179286a4267c03194975cb08bf72827bcb7
Stephane Nicoll <snicoll@pivotal.io>
2018-12-04 17:16:45 +0100

    Remove bom concept from project description

--- initializr-generator/src/main/java/io/spring/initializr/generator/ProjectDescription.java
+++ initializr-generator/src/main/java/io/spring/initializr/generator/ProjectDescription.java
@@ -24,7 +24,6 @@ import io.spring.initializr.generator.buildsystem.BuildSystem;
 import io.spring.initializr.generator.language.Language;
 import io.spring.initializr.generator.packaging.Packaging;
 import io.spring.initializr.generator.util.Version;
-import io.spring.initializr.model.BillOfMaterials;
 import io.spring.initializr.model.Dependency;
 
 /**
@@ -35,8 +34,6 @@ import io.spring.initializr.model.Dependency;
 public class ProjectDescription {
 
 	private final List<Dependency> dependencies = new ArrayList<>();
-
-	private final List<BillOfMaterials> boms = new ArrayList<>();
 
 	private Version springBootVersion;
 
@@ -110,14 +107,6 @@ public class ProjectDescription {
 
 	public List<Dependency> getDependencies() {
 		return Collections.unmodifiableList(this.dependencies);
-	}
-
-	public void addBom(BillOfMaterials bom) {
-		this.boms.add(bom);
-	}
-
-	public List<BillOfMaterials> getBoms() {
-		return Collections.unmodifiableList(this.boms);
 	}
 
 	public String getApplicationName() {
